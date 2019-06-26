26dcf97e593bba400321aaa74ce74389b978d08f
jnaputi <jnaputi253@gmail.com>
2019-06-26 07:05:08 -0500

    Added mongodb and created the Category model

---
+++ Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ShoppingListAPI.Models
+{
+    public class Category
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement(nameof(Name))]
+        [BsonRepresentation(BsonType.String)]
+        public string Name { get; set; }
+    }
+}
